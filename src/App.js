3da8c69e8838429f568b8066eb168be6fe8428d4
Luis Nicanor <luis.nicanor@globant.com>
2023-02-03 22:12:47 -0600

    Get CSV and build table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { usePapaParse } from 'react-papaparse';
 
 function App() {
+  const { readRemoteFile } = usePapaParse();
+
+  const [tableRows, setTableRows] = useState([]);
+
+  const [rows, setRows] = useState([]);
+
+  readRemoteFile('https://data.sfgov.org/api/views/rqzj-sfat/rows.csv', {
+    complete: (results) => {
+      const rowsArray = [];
+      var header = true;
+      results.data.map((target) => {
+        const rowItems = [target[0], target[1], target[2], target[10]];
+        if (header) {
+          setTableRows(rowItems);
+          header = false;
+        }
+        rowsArray.push(rowItems);
+      });
+      setRows(rowsArray);
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="table">
+        <div className="table-header">
+          {tableRows.map((rows, index) => {
+            return <div className="header__item" key={index}>{rows}</div>;
+          })}
+        </div>
+        <div className="table-content">
+        {rows.map((value, index) => {
+            return (
+              <div className="table-row" key={index}>
+                {value.map((val, i) => {
+                  return <div className="table-data" key={i}>{val}</div>;
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
